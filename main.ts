f8e621c847bf0187ff319b2ac194881c4a86dcc7
SalmanKhanK <m.salmank9991@gmail.com>
2022-10-14 19:14:51 +0500

    first code

---
+++ main.ts
@@ -0,0 +1,10 @@
+let a =1;
+console.log(a);
+
+//type asertion
+
+let message;
+message = 'abc';
+let endsWithC = (<string>message).endsWith('g');
+let endsWithD = (message as string).endsWith('g');
+console.log(endsWithC);
